c10e7ad9a6dd0150660109ec5a71f3cf0a17957f
imt17 <157044776+imt17@users.noreply.github.com>
2024-11-27 13:59:42 +0200

    dest

--- backend/destinations/urls.py
+++ backend/destinations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import DestinationList, DestinationDetail
 
+
 urlpatterns = [
     path('', DestinationList.as_view(), name='destination_list'),
     path("<str:name>/", DestinationDetail.as_view(), name="destination-detail"),
